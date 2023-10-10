a53b6806d9593bc80680cf5cfe165107bf29f1cd
Javier Artigas Alfaro <j-a-a1996@hotmail.es>
2023-10-10 23:33:11 +0200

    feat: Addign dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  printWidth: 80,
+};
