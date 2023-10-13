6845cc62fa8242e4755f2e88054e4b1f88454322
Javier Artigas Alfaro <j-a-a1996@hotmail.es>
2023-10-13 11:50:44 +0000

    ci: Estructura e instalando router

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,15 @@ import {Dashboard} from './App'
 import 'bulma/css/bulma.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+
+//TODO: Cambiar Dashboard por el router
+
 root.render(
     <React.StrictMode>
-        <Dashboard />
+    <BrowserRouter>
+           <Dashboard />      
+    </BrowserRouter>
+ 
     </React.StrictMode>,
 )
